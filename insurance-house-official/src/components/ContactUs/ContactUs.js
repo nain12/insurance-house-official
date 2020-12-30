11dddb3e63ac0ca265e515102f6c0ae678995e61
nain12 <nainysewaney@gmail.com>
2020-12-30 18:49:35 +0530

    Contact Us section

--- insurance-house-official/src/components/ContactUs/ContactUs.js
+++ insurance-house-official/src/components/ContactUs/ContactUs.js
@@ -3,9 +3,31 @@ import * as styles from './ContactUs.module.scss';
 
 const ContactUs = () => {
   return (
-        <div className={styles.container}>
-
-        </div>
+      <>
+    <p className={styles.heading}>Contact Us</p>
+       <div className={styles.container}>
+            <div className={styles['contact-form']}>
+            <form>
+                <label id="name">Name</label>
+                <input type="text"/>
+                <label id="address">Address</label>
+                <input type="text"/>
+                <label id="email">Email</label>
+                <input type="text"/>
+                <button>SEND MESSAGE</button>
+            </form>
+            </div>
+            <div className={styles['contact-info']}>
+             <ul>
+                 <li><i className="fa fa-twitter"></i><a href="#">twitter.com</a></li>
+                 <li><i className="fa fa-facebook"></i><a href="#">facebook.com</a></li>
+                 <li><i className="fa fa-instagram"></i><a href="#">instagram.com</a></li>
+                 <li><i className="fa fa-envelope"></i><p>deepuvalecha27@gmail.com</p></li>
+                 <li><i className="fa fa-home"></i><p>Office No.1, Laxmi Apartment, Baba Dholuram Darbar Road, Near Satyam Medical Store, Ulhasnagar- 421 001.</p></li>
+             </ul>
+            </div>
+       </div>
+       </>
   )
 }
 
